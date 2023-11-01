bdd70b856d18c2ac81db7a52a781209c977e349e
justin <justin.ng26@hotmail.com>
2023-11-01 16:51:50 +0000

    add auth

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">nextjs 13 were coming</h1>
-      <h1 className="h2-bold">nextjs 13 were coming</h1>
-    </div>
-  );
-}
