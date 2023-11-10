9aa789301c369af06e6bb343b2167398aca2075b
justin <justin.ng26@hotmail.com>
2023-11-10 18:44:08 +0000

    implemnted mobileNav

---
+++ components/shared/navbar/MobileNav.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { SignedOut } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetClose,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { sidebarLinks } from "@/constants";
+
+const NavContent = () => {
+  return (
+    <section>
+        {sidebarLinks.map((link) => (   
+            <SheetClose>
+                
+            </SheetClose>}
+    </section>
+  )
+};
+
+const MobileNav = () => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Image
+          src="/assets/icons/hamburger.svg"
+          width={36}
+          height={36}
+          alt="menu"
+          className="invert-colors sm:hidden"
+        />
+      </SheetTrigger>
+
+      <SheetContent
+        side="left"
+        className="background-light900_dark200 border-none"
+      >
+        <Link href="/" className="flex items-center gap-1">
+          <Image
+            src="/assets/images/site-logo.svg"
+            width={23}
+            height={23}
+            alt="Deflow Logo"
+          />
+
+          <p className="h2-bold text-dark100_light900 font-spaceGrotesk">
+            Dev <span className="text-primary-500">Overflow</span>
+          </p>
+        </Link>
+
+        <div>
+          <SheetClose asChild>
+            <NavContent />
+          </SheetClose>
+
+          {/* When signed out */}
+
+          <SignedOut>
+            <div className="flex flex-col gap-3">
+              <SheetClose asChild>
+                <Link href="/sign-in">
+                  <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                    <span className="primary-text-gradient">Sign In</span>
+                  </Button>
+                </Link>
+              </SheetClose>
+
+              <SheetClose asChild>
+                <Link href="/sign-up">
+                  <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                    Sign up
+                  </Button>
+                </Link>
+              </SheetClose>
+            </div>
+          </SignedOut>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
+export default MobileNav;
