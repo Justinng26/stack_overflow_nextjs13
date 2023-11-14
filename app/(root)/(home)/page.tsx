bdc5989a47890c826a0276a37b65f589808c521b
justin <justin.ng26@hotmail.com>
2023-11-14 17:55:01 +0000

    implement local globalsearchbar

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,3 +1,29 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <div className="flex w-full flex-col-reverse">
+        <h1 className="h1-bold text-dark100_light900">All questions</h1>
+
+        <Link href="/ask-questions" className="flex justify-end max-sm:w-full">
+          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+            Ask a question
+          </Button>
+        </Link>
+      </div>
+
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchbar
+          route="/"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for questions"
+        />
+        Filters
+      </div>
+    </>
+  );
 }
