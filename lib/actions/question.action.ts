8f01a38e61dc0786aee55e917d6ed9e840f07851
justin <justin.ng26@hotmail.com>
2023-11-27 13:30:14 +0000

    work on question.action

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -1,10 +1,47 @@
 "use server";
 
+import Question from "@/database/question.model";
 import { connectToDatabase } from "../mongoose";
+import Tag from "@/database/tag.model";
 
 export async function createQuestion(params: any) {
   // eslint-disable-next-line no-undef
   try {
     connectToDatabase();
+
+    const { title, content, tags, author, path } = params;
+
+    const question = await Question.create({
+      title,
+      content,
+      author,
+    });
+
+    const tagDocuments = [];
+
+    // create the tags or get them if they already exist
+    // for each tag, push the question id to the questions array
+    // then push the tag id to the tagDocuments array
+    // then update the question with the tagDocuments array
+    // this is done to avoid having to do two separate queries
+    // one to create the tags and one to update the question
+    // a question can have many tags and a tag can have many questions, that is the goal.
+
+    for (const tag of tags) {
+      const existingTag = await Tag.findOneAndUpdate(
+        { name: { $regex: new RegExp(`^${tag}$`, "i") } },
+        { $setOnInsert: { name: tag }, $push: { question: question._id } },
+        { upsert: true, new: true }
+      );
+      tagDocuments.push(existingTag._id);
+    }
+
+    await Question.findByIdAndUpdate(question._id, {
+      $push: { tags: { $each: tagDocuments } },
+    });
+
+    // create an interaction record for user's ask_question action
+
+    // increment author's reputation by 5
   } catch (error) {}
 }
