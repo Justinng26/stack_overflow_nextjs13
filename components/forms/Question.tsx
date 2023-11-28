fb4336186af8cf218175b407470bb583e3fa27b7
justin <justin.ng26@hotmail.com>
2023-11-28 14:14:18 +0000

    successfully submitted first question to mongoDB database

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -21,12 +21,19 @@ import { QuestionsSchema } from "@/lib/validations";
 import Image from "next/image";
 import { Badge } from "../ui/badge";
 import { createQuestion } from "@/lib/actions/question.action";
+import { useRouter, usePathname } from "next/navigation";
 
 const type: any = "create";
 
-const Question = () => {
+interface Props {
+  mongoUserId: string;
+}
+
+const Question = ({ mongoUserId }: Props) => {
   const editorRef = useRef(null);
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const router = useRouter();
+  const pathname = usePathname();
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof QuestionsSchema>>({
@@ -52,8 +59,10 @@ const Question = () => {
         title: values.title,
         content: values.explanation,
         tags: values.tags,
-        //  author:
+        author: JSON.parse(mongoUserId),
       });
+
+      router.push("/");
     } catch (error) {
     } finally {
       setIsSubmitting(false);
