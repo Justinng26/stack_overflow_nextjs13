0576594e1ed4e6b60ff0e52f1c6430b24cf26e53
justin <justin.ng26@hotmail.com>
2023-11-30 18:43:24 +0000

    resolved issue in sheet component

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -7,12 +7,6 @@ import { X } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 
-// Hypothetical type definition
-type DialogPortalProps = {
-  className: string;
-  // ... other properties
-};
-
 const Sheet = SheetPrimitive.Root;
 
 const SheetTrigger = SheetPrimitive.Trigger;
