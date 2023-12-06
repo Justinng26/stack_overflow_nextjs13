8b431401a285f891b401dc2fbb636f5b20e810e6
justin <justin.ng26@hotmail.com>
2023-12-06 17:38:13 +0000

    implemented parsing and displaying content on question details page

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import Metric from "@/components/shared/Metric";
 import { formatAndDivideNumber, getTimestamp } from "@/lib/utils";
 import ParseHTML from "@/components/shared/ParseHTML";
+import RenderTag from "@/components/shared/RenderTag";
+import Answer from "@/components/forms/Answer";
 
 const Page = async ({ params, searchParams }) => {
   const result = await getQuestionById({ questionId: params.id });
@@ -59,6 +61,19 @@ const Page = async ({ params, searchParams }) => {
       </div>
 
       <ParseHTML data={result.content} />
+
+      <div className="mt-8 flex flex-wrap gap-2">
+        {result.tags.map((tag: any) => (
+          <RenderTag
+            key={tag.id}
+            name={tag.name}
+            _id={tag._id}
+            showCount={false}
+          />
+        ))}
+      </div>
+
+      <Answer />
     </>
   );
 };
