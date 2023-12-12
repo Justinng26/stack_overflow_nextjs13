5646791eafefcc8662083f8351451f5e20b8982f
justin <justin.ng26@hotmail.com>
2023-12-12 15:47:38 +0000

    display saved questions on collection page

--- app/(root)/collection/page.tsx
+++ app/(root)/collection/page.tsx
@@ -2,26 +2,26 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import Filter from "@/components/shared/Filter";
-import { HomePageFilters } from "@/constants/filters";
-import HomeFilters from "@/components/home/HomeFilters";
+import { QuestionFilters } from "@/constants/filters";
+
 import NoResult from "@/components/shared/NoResult";
 import QuestionCard from "@/components/cards/QuestionCard";
-import { getQuestions } from "@/lib/actions/question.action";
+
+import { getSavedQuestions } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs";
 
 export default async function Home() {
-  const result = await getQuestions({});
+  const { userId } = auth();
+
+  if (!userId) return null;
+
+  const result = await getSavedQuestions({
+    clerkId: userId,
+  });
 
   return (
     <>
-      <div className="flex w-full flex-col-reverse">
-        <h1 className="h1-bold text-dark100_light900">All questions</h1>
-
-        <Link href="/ask-question" className="flex justify-end max-sm:w-full">
-          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
-            Ask a question
-          </Button>
-        </Link>
-      </div>
+      <h1 className="h1-bold text-dark100_light900">Saved questions</h1>
 
       <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
         <LocalSearchbar
@@ -31,12 +31,10 @@ export default async function Home() {
           placeholder="Search for questions"
         />
         <Filter
-          filters={HomePageFilters}
+          filters={QuestionFilters}
           otherClasses="min-h-[56px] sm:min-w-[170px]"
-          containerClasses="hidden max-md:flex"
         />
       </div>
-      <HomeFilters />
 
       <div className="mt-10 flex w-full flex-col gap-6">
         {result.questions.length > 0 ? (
@@ -55,7 +53,7 @@ export default async function Home() {
           ))
         ) : (
           <NoResult
-            title="There's no question to show"
+            title="There's no saved questions to show"
             description="Be the first to break the silence! Ask a Question and kickstart the
           discussion. Our query could be the next big thing others learn from. Get
           involved!"
