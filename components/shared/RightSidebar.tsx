20802630494b0b062bb6b4b98d7e96f63e1713f2
justin <justin.ng26@hotmail.com>
2023-12-16 19:07:46 +0000

    rendered the top questions on right sidebar

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -2,30 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import RenderTag from "./RenderTag";
-
-const hotQuestions = [
-  {
-    _id: "1",
-    title: "How to use the new Next.js Image Component?",
-  },
-  {
-    _id: "2",
-    title: "cascading deletes in laravel?",
-  },
-
-  {
-    _id: "3",
-    title: "How do I express custom server logic in Next.js?",
-  },
-  {
-    _id: "4",
-    title: "how to perfectly center a div using tailwind?",
-  },
-  {
-    _id: "5",
-    title: "Best practices for using Next.js Image Component?",
-  },
-];
+import { getHotQuestions } from "@/lib/actions/question.action";
 
 const popularTags = [
   { _id: "1", name: "javascript", totalQuestions: 5 },
@@ -35,7 +12,8 @@ const popularTags = [
   { _id: "5", name: "redux", totalQuestions: 10 },
 ];
 
-const RightSidebar = () => {
+const RightSidebar = async () => {
+  const hotQuestions = await getHotQuestions();
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
       <div>
@@ -43,7 +21,7 @@ const RightSidebar = () => {
         <div className="mt-7 flex w-full flex-col gap-[30px]">
           {hotQuestions.map((question) => (
             <Link
-              href={`/questions/${question._id}`}
+              href={`/question/${question._id}`}
               key={question._id}
               className="flex cursor-pointer items-center justify-between gap-7"
             >
