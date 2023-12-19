0042b7dc7453c7bddeec32b0aadb426ae52e1d8e
justin <justin.ng26@hotmail.com>
2023-12-19 18:50:57 +0000

    working on filter for community page

--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -2,10 +2,36 @@
 
 import { HomePageFilters } from "@/constants/filters";
 import { Button } from "../ui/button";
-import React from "react";
+import React, { useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { formUrlQuery } from "@/lib/utils";
 
 const HomeFilters = () => {
-  const active = "frequent";
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const [active, setActive] = useState("");
+
+  const handleTypeClick = (item: string) => {
+    if (active === item) {
+      setActive("");
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: null,
+      });
+
+      router.push(newUrl, { scroll: false });
+    } else {
+      setActive(item);
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: item.toLowerCase(),
+      });
+
+      router.push(newUrl, { scroll: false });
+    }
+  };
 
   return (
     <div className="mt-10 flex-wrap gap-3 md:flex">
@@ -18,6 +44,7 @@ const HomeFilters = () => {
               ? "bg-primary-100 text-primary-500"
               : "bg-light-800 text-light-500"
           }`}
+          onClickCapture={() => handleTypeClick(item.value)}
         >
           {item.name}
         </Button>
