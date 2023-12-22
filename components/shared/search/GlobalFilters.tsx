65c618ee00131ec5f9fecf0070e91634aae75bbd
justin <justin.ng26@hotmail.com>
2023-12-22 18:03:22 +0000

    implemeted global result component

--- components/shared/search/GlobalFilters.tsx
+++ components/shared/search/GlobalFilters.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { GlobalSearchFilters } from "@/constants/filters";
+import { formUrlQuery } from "@/lib/utils";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useState } from "react";
 
@@ -11,6 +12,29 @@ const GlobalFilters = () => {
   const typeParams = searchParams.get("type");
 
   const [active, setActive] = useState(typeParams || "");
+
+  const handleTypeClick = (item: string) => {
+    if (active === item) {
+      setActive("");
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "type",
+        value: null,
+      });
+
+      router.push(newUrl, { scroll: false });
+    } else {
+      setActive(item);
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "type",
+        value: item.toLowerCase(),
+      });
+
+      router.push(newUrl, { scroll: false });
+    }
+  };
+
   return (
     <div className="flex items-center gap-5 px-5">
       <p className="text-dark400_light900 body-medium">Type: </p>
@@ -27,6 +51,7 @@ const GlobalFilters = () => {
                     : "bg-light-700 text-dark-400 hover:text-primary-500 dark:bg-dark-500"
                 }
               `}
+            onClick={() => handleTypeClick(item.value)}
           >
             {item.name}
           </button>
